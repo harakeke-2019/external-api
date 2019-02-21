0aaf0d71eeac320e504cc3d8d21d664c81821e58
Micah Nathaniel Titus Peautagi <micahpeautagi@gmail.com>
2019-02-21 21:42:45 +1300

    Re-arranged components / linked to puppies API

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,35 +1,15 @@
 import React from 'react'
-import {getPeople} from '../api'
+import {HashRouter as Router, Route} from 'react-router-dom'
+import puppy from './puppy'
 
-class App extends React.Component {
-  constructor () {
-    super()
-    this.state = {
-      name: ''
-    }
-  }
-
-  componentDidMount () {
-    const personId = 1
-    getPeople(personId)
-      .then(res => {
-        this.setState({name: res.name})
-      })
-      .catch(err => {
-        console.error('Error:', err)
-      })
-  }
-
-  // If you want to test that the UI (Divs) are in the right place you would use Enzyme. Because enzyme cares about the markup/UI.
-
-  render () {
-    return (
-      <div>
-        <h1>Hello earth people</h1>
-        <h3>Api Name: {this.state.name}</h3>
+const App = () => {
+  return (
+    <Router>
+      <div className='wrapper'>
+        <Route exact path='/puppy' component={puppy}/>
       </div>
-    )
-  }
+    </Router>
+  )
 }
 
 export default App
